499a85e883dbdf2ea91dd82aa5a3cae5b30c0846
lucasaraujods <lucasara1707@gmail.com>
2024-06-27 20:20:13 -0300

    Exercicio 01

---
+++ exercicio01/ex01.js
@@ -0,0 +1,7 @@
+//Faça um programa que leia o nome de uma pessoa e mostre uma mensagem de boasvindas para ela:
+//Ex:
+//Qual é o seu nome? João da Silva
+//Olá João da Silva, é um prazer te conhecer!
+
+let nome = window.prompt("Informe o seu nome: ")
+document.write(`Olá ${nome} , é um prazer te conhecer, seja Bem-vindo`)
