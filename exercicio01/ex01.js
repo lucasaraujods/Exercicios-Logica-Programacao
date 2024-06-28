c3868605729f060b8c766cc19c5336aeb3b09f5e
lucasaraujods <lucasara1707@gmail.com>
2024-06-27 21:39:14 -0300

    com a nova menssagem

--- exercicio01/ex01.js
+++ exercicio01/ex01.js
@@ -2,6 +2,7 @@
 //Ex:
 //Qual é o seu nome? João da Silva
 //Olá João da Silva, é um prazer te conhecer!
+//testando a mudança nos comentarios
 
 let nome = window.prompt("Informe o seu nome: ")
 document.write(`Olá ${nome} , é um prazer te conhecer, seja Bem-vindo`)
