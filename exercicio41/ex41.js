7869289d73b4b63509aac7ad4e6c19be9cc6f56c
lucasaraujods <lucasara1707@gmail.com>
2024-07-02 18:11:17 -0300

    exercicio tabuada do 5

---
+++ exercicio41/ex41.js
@@ -0,0 +1,29 @@
+//Faça um algoritmo para Imprimir a tabuada do número 5, usando while
+
+let numero=0
+console.log("Adição")
+while(numero<=10){
+    console.log(`${numero} + 5 = ${numero+5}`)
+    numero++
+}
+
+numero =0
+console.log("Subtração:")
+while(numero<=10){
+    console.log(`${numero} - 5 = ${numero-5}`)
+    numero++
+}
+
+numero=0
+console.log("Multiplicação:")
+while(numero<=10){
+    console.log(`${numero} x 5 = ${numero*5}`)
+    numero++
+}
+
+numero=0
+console.log("Divisão")
+while(numero<=10){
+    console.log(`${numero} / 5 = ${numero/5}`)
+    numero++
+}
