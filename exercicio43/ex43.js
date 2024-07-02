6be347b72fe7c1fb1c521fa74bbc09758edf1e86
lucasaraujods <lucasara1707@gmail.com>
2024-07-02 18:59:09 -0300

    exercicio simples de contagem usando while

---
+++ exercicio43/ex43.js
@@ -0,0 +1,9 @@
+//Desenvolva um programa que mostre na tela a seguinte contagem:
+//100 95 90 85 80 ... 0 Acabou!
+
+let numero = 100
+
+while(numero>=0){
+    console.log(numero)
+    numero-= 5 
+}
