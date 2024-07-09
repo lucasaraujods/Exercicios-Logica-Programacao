073580c51613482d77e300b288c2a9aec6dd4c22
lucasaraujods <lucasara1707@gmail.com>
2024-07-09 18:28:47 -0300

    exercicio simples usando for

---
+++ exercicio68/ex68.js
@@ -0,0 +1,7 @@
+//Faça um algoritmo que receba um valor, inteiro e positivo, e usando a estrutura FOR, mostre no console uma contagem de 0 até o número recebido.
+
+let numero = parseInt(window.prompt("Informe o número para saber sua contagem"))
+
+for( let i= 0; i<=numero; i++){
+    console.log(i)
+}
