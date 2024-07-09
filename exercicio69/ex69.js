2be4ee43af199c16561fc43a2c0a490e75863f96
lucasaraujods <lucasara1707@gmail.com>
2024-07-09 18:31:27 -0300

    exercicio simples usando for

---
+++ exercicio69/ex69.js
@@ -0,0 +1,6 @@
+//Desenvolva um algoritmo que usando a estrutura FOR mostre no console uma contagem de 0 a 50, porém, essa contagem tem que ser feita de 5 em 5.
+//Ex.: 0 5 10 15 20 25 … 45 50 Fim!
+
+for( let i=0; i<=50; i+=5){
+    console.log(i)
+}
